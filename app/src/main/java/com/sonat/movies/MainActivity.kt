414915a43cd746d4bd09ea3200bb4680534da08c
sonat <sonatdev@gmail.com>
2020-11-24 17:51:11 +0300

    Add Movie Details Activity

--- app/src/main/java/com/sonat/movies/MainActivity.kt
+++ app/src/main/java/com/sonat/movies/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.sonat.movies
 
-import android.os.Bundle
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<TextView>(R.id.main_activity_text_view).apply {
+            setOnClickListener { openMovieDetails() }
+        }
+    }
+
+    private fun openMovieDetails() {
+        startActivity(Intent(this, MovieDetailsActivity::class.java))
     }
 }
