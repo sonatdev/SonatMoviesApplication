7b175d9896f1266a24f7c97ff644e04dee0621fc
sonat <45698825+sonatdev@users.noreply.github.com>
2020-11-30 17:46:45 +0300

    Add Movie Details Activity (#1)

---
+++ app/src/main/java/com/sonat/movies/MovieDetailsActivity.kt
@@ -0,0 +1,6 @@
+package com.sonat.movies
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MovieDetailsActivity : AppCompatActivity(R.layout.activity_movie_details)
