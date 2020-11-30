882c4d5d599ab4180f60314493684da561540a59
sonat <sonatdev@gmail.com>
2020-11-30 15:37:52 +0300

    Add Movies and Movies Details fragments

--- app/src/main/java/com/sonat/movies/MovieDetailsActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.sonat.movies
-
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
-
-class MovieDetailsActivity : AppCompatActivity(R.layout.activity_movie_details)
